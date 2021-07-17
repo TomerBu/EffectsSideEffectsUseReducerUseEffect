ae477239c89b0d56b16b46c214cea91a1bb8b35b
TomerBu <tomer.bu@gmail.com>
2021-07-17 13:28:10 +0300

    done

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import AuthContext from '../store/auth-context'
+import AuthContext from '../store/auth-context';
 import styles from './Navigation.module.css';
 
 const Navigation = (props) => {
