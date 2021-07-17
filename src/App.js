be801b88a29b16ed0e749cce9ee37f9d5a208bf3
TomerBu <tomer.bu@gmail.com>
2021-07-17 13:10:10 +0300

    step 2 - prividing the context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -26,13 +27,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home />}
       </main>
-    </Fragment>
+    </AuthContext.Provider >
   );
 }
 
