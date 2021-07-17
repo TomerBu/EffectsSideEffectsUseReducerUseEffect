1b8625323844adcbd410ef0438d5f606830b2a1a
TomerBu <tomer.bu@gmail.com>
2021-07-17 13:51:23 +0300

    passing functions with context

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import AuthContext from '../store/auth-context';
 import styles from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext)
   return (
     <nav className={styles.nav}>
@@ -19,7 +19,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.logoutHandler}>Logout</button>
           </li>
         )}
       </ul>
