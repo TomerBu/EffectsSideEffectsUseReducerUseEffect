fb935cfface9b7ee6dfd1825b8fe702a06c2e8ce
TomerBu <tomer.bu@gmail.com>
2021-07-21 12:53:21 +0300

    moved all context related work from app.js to auth-context.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import React, { useState, useEffect } from 'react';
-
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import { Fragment, useContext } from 'react';
 import AuthContext from './components/store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  useEffect(() => {
-    const storageIsLoggedIn = localStorage.getItem('isLoggedIn')
-
-    if (storageIsLoggedIn === 'true') {
-      setIsLoggedIn(true)
-    }
-  }, [])
-  const loginHandler = (email, password) => {
-    // We should of course check email and password - DEMO
-    localStorage.setItem("isLoggedIn", "true")
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext)
   return (
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, logoutHandler: logoutHandler}}>
-      <MainHeader/>
+    <Fragment>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home />}
+        {!ctx.isLoggedIn && <Login onLogin={ctx.loginHandler} />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider >
+    </Fragment>
   );
 }
 
